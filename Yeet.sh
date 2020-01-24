682f90fdfb62d7a455caedf42d9d73bff2b511f6
HGalloway <52604068+HGalloway@users.noreply.github.com>
2020-01-24 09:59:11 -0600

    Create Yeet.sh

---
+++ Yeet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "_____________________"
+echo "___      YEET     ___"
+echo "_____________________"
+echo "You better run this as root or imma smack you"
+echo "What the hell do you want?"
+echo "Enter everything as written"
+echo "1.Password Policy"
+echo "5.Gun Its Cheeks"
