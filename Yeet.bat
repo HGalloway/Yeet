f80c1d180076c15dcc5085485f32d6107d5f4e84
HGalloway <52604068+HGalloway@users.noreply.github.com>
2020-01-28 10:54:51 -0600

    Update Yeet.bat

--- Yeet.bat
+++ Yeet.bat
@@ -8,9 +8,11 @@ set /p wdyw= What the hell do you want to do?
 echo "Enter everything as written"
 echo "1.Password Policy"
 echo "2.User Authentication"
+echo "3.Uninstall Unneeded Program"
 echo "5.Gun Its Cheeks"
 if "%wdyw%"=="Password policy" GOTO:PASSPOL
 if "%wdyw%"=="User Authentication" GOTO:USERAUTH
+if "%wdyw%"=="Uninstal Unneeded Programs" GOTO:UUP
 if "%wdyw%"=="Gun its Cheeks" GOTO:GUNITSCHEEKS
 :PASSPOL
 echo "1.Password Policy"
@@ -25,19 +27,22 @@ echo "Setting minimum password age."
 net accounts /minpwage:10
 echo "Setting minimum length."
 net accounts /minpwlen: 12
+
 :USERAUTH
 echo "Do you want to add or delete a user"
 echo "If you don't want to then enter 'Nope'"
 set /p aod=""
 if "%aod%"=="Delete" goto:Delete
 if "%aod%"=="Add" goto:Add
+if "%aod%"=="Nope" goto:YEET
 :Add 
 echo "Name of user you want to add:"
 set /p add=""
 set /p password="Password:"
 echo "So you want to add a user by the name of %add% and with the password of %password%"
-set /p yos=""
-if "%%"
+set /p yon=""
+if "%yon%"=="Yes" net user %add% %password% /add
+if "%yon%"=="No" goto:USERAUTH
 :Delete
 echo "Users:"
 net users
@@ -48,7 +53,11 @@ set /p yon=""
 if "%yon%"=="Yes" net user "%del%" /delete
 goto:USERAUTH
 if "%yon%"=="No" goto:USERAUTH
-if "%aod%"=="Nope" goto:YEET
+
+:UUP
+wmic product get name
+rem https://www.digitalcitizen.life/six-ways-removeuninstall-windows-programs-and-apps
+
 :GUNITSCHEEKS
 echo "1.Password Policy"
 echo " "
