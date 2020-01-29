2cace8fc74ce16adeeae64b4d4490dcf6923fcaf
HGalloway <52604068+HGalloway@users.noreply.github.com>
2020-01-29 14:33:01 -0600

    Update Yeet.bat

--- Yeet.bat
+++ Yeet.bat
@@ -4,16 +4,17 @@ echo "_____________________"
 echo "___      YEET     ___"
 echo "_____________________"
 echo "You better run this as root or imma smack you"
-set /p wdyw= What the hell do you want to do?
 echo "Enter everything as written"
 echo "1.Password Policy"
 echo "2.User Authentication"
 echo "3.Uninstall Unneeded Program"
 echo "5.Gun Its Cheeks"
+set /p wdyw= What the hell do you want to do?
 if "%wdyw%"=="Password policy" GOTO:PASSPOL
 if "%wdyw%"=="User Authentication" GOTO:USERAUTH
 if "%wdyw%"=="Uninstal Unneeded Programs" GOTO:UUP
 if "%wdyw%"=="Gun its Cheeks" GOTO:GUNITSCHEEKS
+if "%wdyw%"=="Nothing" Exit
 :PASSPOL
 echo "1.Password Policy"
 echo " "
@@ -26,8 +27,11 @@ net accounts /uniquepw:5
 echo "Setting minimum password age."
 net accounts /minpwage:10
 echo "Setting minimum length."
-net accounts /minpwlen: 12
-
+net accounts /minpwlen:12
+echo ""
+net accounts
+pause
+goto:YEET
 :USERAUTH
 echo "Do you want to add or delete a user"
 echo "If you don't want to then enter 'Nope'"
@@ -42,6 +46,8 @@ set /p password="Password:"
 echo "So you want to add a user by the name of %add% and with the password of %password%"
 set /p yon=""
 if "%yon%"=="Yes" net user %add% %password% /add
+pause
+goto:USERAUTH
 if "%yon%"=="No" goto:USERAUTH
 :Delete
 echo "Users:"
@@ -51,6 +57,7 @@ set /p del=""
 echo "Are you sure you want to delete %del%" 
 set /p yon=""
 if "%yon%"=="Yes" net user "%del%" /delete
+pause
 goto:USERAUTH
 if "%yon%"=="No" goto:USERAUTH
 
